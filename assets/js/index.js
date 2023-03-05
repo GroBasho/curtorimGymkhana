afaadfdf5793c2de8bd8973aa86781a7dbf04900
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2023-03-05 23:59:50 +0530

    feat: pop-up-notice

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,24 @@
+$(function(){
+    var overlay = $('<div id="overlay"></div>');
+    overlay.show();
+    overlay.appendTo(document.body);
+    $('.popup').show();
+    $('.close').click(function(){
+    $('.popup').hide();
+    overlay.appendTo(document.body).remove();
+    return false;
+    });
+    
+    
+     
+    
+    $('.x').click(function(){
+    $('.popup').hide();
+    overlay.appendTo(document.body).remove();
+    return false;
+    });
+    });
+
 document.addEventListener("DOMContentLoaded", function () {
     function share() {
         this.classList.toggle('active');
